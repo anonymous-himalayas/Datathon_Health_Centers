b757ddce7799c8df7732b0049423743c66047e1e
Himal <himalm100@gmail.com>
2025-04-13 02:09:51 -0700

    figure out deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss  from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://anonymous-himalayas.github.io/Datathon_Health_Centers'
+  base: '/Datathon_Health_Centers/',
 })
